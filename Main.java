b8e58d330a03d00b36ca8825e8eac33568e9a7e5
Dimon <dmitriy.popov1992@gmail.com>
2023-04-06 21:54:38 +0300

    homework final

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int sum1 = a + b;
+        int sum2 = c + d;
+
+        System.out.println(sum1 > sum2);
+        sum1 = +1;
+        sum2 = -2;
+
+        System.out.println(sum1 > sum2);
+        System.out.println(sum1 % 2 == 0 || sum2 % 2 == 0);
+    }
+}
